f617f3b27fdc1abaf4522fa77a95a63f3e365a3d
kevin briand <kevinbriand4@gmail.com>
2021-06-11 19:51:11 -0400

    Add: partie bd

---
+++ backend/src/BaseDeDonnees/CreerCompte.js
@@ -0,0 +1,11 @@
+
+export async function CreerCompte(p_baseDonnees, p_nomClient, p_motDePasse){
+    
+    await p_baseDonnees.collection('InformationsClient').insertOne(
+        {
+            nom : p_nomClient,
+            motDePasse : p_motDePasse
+        }
+    );
+}
+
